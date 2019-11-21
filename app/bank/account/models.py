7d55bd070ea0fd5b8d9179fabffd462fc2fed6fc
geekwise-deidra-wallert <geekwise.deidra.wallert@gmail.com>
2019-11-21 21:38:37 +0000

    created account models

--- app/bank/account/models.py
+++ app/bank/account/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 class Account (models.Model):
-    
+    account_choices = (
+        ('checking', 'CHECKING'),
+        ('savings', 'SAVINGS'),
+        ('none', 'NONE'),
+    )
+    account_choices_params = models.CharField(
+        max_length = 8,
+        choices = account_choices,
+        default= account_choices[0]
+    )
+
+    def __str__(self):
+        return (
+            f"{self.account_choices}"
+        )
